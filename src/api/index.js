c50b12b066ffeb30c3e0abccee4fc23d53b95d49
abubakarzia <abubakar.zia1998@gmail.com>
2022-06-22 17:44:22 +0500

    Updated Backend Live Link

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://memories992.herokuapp.com/' });
+// const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
